f657e9a39dc09c924744662e6c4bddeb1496cac4
Rakshita Goyal <goyal31rakshita@gmail.com>
2025-12-31 11:51:14 +0530

    multiply committed

---
+++ multiply.java
@@ -0,0 +1,10 @@
+package gitPractice;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int product = a * b;
+        System.out.println("The product is: " + product);   
+    }
+}
